5a973038df96118cfb1f324765d100e27e677139
Trinkas <jojotri@hotmail.com>
2020-11-21 19:16:20 +0000

    Update ThreadMsg.java

--- TP_PD_CLIENT/src/pt/isec/tppd/g24/ThreadMsg.java
+++ TP_PD_CLIENT/src/pt/isec/tppd/g24/ThreadMsg.java
@@ -35,9 +35,60 @@ public class ThreadMsg extends Thread{
                     System.out.println(msg.getUsername() + ": " + msg.getConteudo());
                 }
                 System.out.print("> ");
+            }catch (SocketException e) {
+                synchronized (socketTcp) {
+                    int i = 0;
+                    ByteArrayOutputStream bOut;
+                    ObjectOutputStream out;
+                    DatagramPacket packet = null;
+                    ByteArrayInputStream bIn;
+                    ObjectInputStream iin;
+                    MsgServer resposta;
+                    boolean conexao = false;
+                    do {
+                        try {
+                            bOut = new ByteArrayOutputStream();
+                            out = new ObjectOutputStream(bOut);
+                            out.writeUnshared(serverRequest);
+                            out.flush();
+
+                            packet = new DatagramPacket(bOut.toByteArray(), bOut.size(), InetAddress.getByName(lista.get(i).getAddr()), lista.get(i).getPortUdp());
+
+                            socketUdp.send(packet);
+                            socketUdp.setSoTimeout(5000); // 5 sec
+                            packet = new DatagramPacket(new byte[MAX_SIZE], MAX_SIZE);
+                            socketUdp.receive(packet);
+
+                            bIn = new ByteArrayInputStream(packet.getData(), 0, packet.getLength());
+                            iin = new ObjectInputStream(bIn);
+
+                            resposta = (MsgServer) iin.readObject();
+
+                            conexao = resposta.getPodeLigar();
+                            lista = resposta.getAddrString();
+                            i = 0;
+                        } catch (SocketTimeoutException socketTimeoutException) {
+                            i++;
+                            if (i == lista.size()) {
+                                System.out.println();
+                                System.out.println("Conexao com os servidores perdida.");
+                                System.out.flush();
+                                System.out.println();
+                                System.exit(-1);
+                            }
+                        } catch (IOException ioException) {
+                        } catch (ClassNotFoundException classNotFoundException) {
+                        }
+                    } while (!conexao);
+                    try {
+                        socketTcp = new Socket(lista.get(0).getAddr(), lista.get(0).getPortTcp());
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                }
             } catch (IOException e) {
-                System.out.println();
-                System.out.println("Impossibilidade de aceder ao conteudo da mensagem recebida! " + e);
+                System.out.println("ThreadMsg!" + e);
+                System.out.println(e);
             } catch (ClassNotFoundException e) {
                 System.out.println();
                 System.out.println("Mensagem recebida de tipo inesperado! " + e);
